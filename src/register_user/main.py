80dda625c665e391ad92f7cc5e029eff339b8c49
Luis Tapia <luitapia307@gmail.com>
2026-04-23 16:32:05 -0400

    Adição de identificar se o usuário já existe. Função de logar com usuário existente, e função de deletar conta

---
+++ src/register_user/main.py
@@ -0,0 +1,84 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from src.shared import functions
+from src.constants.url import URL
+
+
+class User:
+    def __init__(self):
+        self.driver, self.wait = functions.iniciar_driver(URL)
+
+    def ir_para_login(self):
+        login_link = self.wait.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[href="/login"]'))
+        )
+        print(f"Navegando para: {login_link.text}")
+        login_link.click()
+
+    def selecionar_mr(self):
+        functions.find_click(self.wait, 'label[for="id_gender1"]')
+
+    def realizar_signup(self, user):
+        functions.find_tap(self.wait, '[name="name"]', user["Name"], by=By.CSS_SELECTOR)
+        functions.find_tap(self.wait, '[data-qa="signup-email"]', user["Email"], by=By.CSS_SELECTOR)
+        functions.find_click(self.wait, 'button[data-qa="signup-button"]')
+        print("Botão Signup clicado!")
+
+    def usuario_ja_existe(self):
+        try:
+            WebDriverWait(self.driver, 3).until(
+                EC.visibility_of_element_located(
+                    (By.XPATH, "//*[contains(text(), 'already exist')]")
+                )
+            )
+            return True
+        except:
+            return False
+
+    def preencher_conta(self, user):
+        functions.find_tap(self.wait, '[data-qa="password"]', user["password"])
+
+        functions.select_option(self.wait, "days", user["birthday"], mode="value")
+        functions.select_option(self.wait, "months", user["birthmonth"], mode="text")
+        functions.select_option(self.wait, "years", user["birthyear"], mode="value")
+
+        if user["Singin"]:
+            functions.find_click(self.wait, "#newsletter")
+
+        if user["Receive"]:
+            functions.find_click(self.wait, "#optin")
+
+    def preencher_endereco(self, user):
+        functions.find_tap(self.wait, "#first_name", user["First Name"])
+        functions.find_tap(self.wait, "#last_name", user["Last Name"])
+        functions.find_tap(self.wait, "#address1", user["Address"])
+        functions.select_option(self.wait, "country", user["Country"], mode="text")
+        functions.find_tap(self.wait, "#state", user["State"])
+        functions.find_tap(self.wait, "#city", user["City"])
+        functions.find_tap(self.wait, "#zipcode", user["Zipcode"])
+        functions.find_tap(self.wait, "#mobile_number", user["phone"])
+
+    def criar_conta(self):
+        functions.find_click(self.wait, '[data-qa="create-account"]')
+        continue_link = self.wait.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-qa="continue-button"]'))
+        )
+        continue_link.click()
+        print('O botão foi clicado')
+
+
+    def delete_account(self):
+        delete_link = self.wait.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[href="/delete_account"]'))
+        )
+        print(f"Navegando para: {delete_link.text}")
+        delete_link.click()
+        continue_link = self.wait.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-qa="continue-button"]'))
+        )
+        continue_link.click()
+        print('O botão foi clicado')
+
+        
---
+++ src/shared/functions.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def iniciar_driver(url):
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)
+
+    prefs = {
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False,
+        "profile.password_manager_leak_detection": False,
+        "autofill.profile_enabled": False,
+        "autofill.credit_card_enabled": False
+    }
+    options.add_experimental_option("prefs", prefs)
+
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    wait = WebDriverWait(driver, 10)
+
+    return driver, wait
+
+
+def get_element(wait, selector, by=By.CSS_SELECTOR):
+    return wait.until(
+        EC.visibility_of_element_located((by, selector))
+    )
+
+
+def find_click(wait, selector, by=By.CSS_SELECTOR):
+    element = wait.until(
+        EC.element_to_be_clickable((by, selector))
+    )
+    element.click()
+
+
+def find_tap(wait, selector, text="", by=By.CSS_SELECTOR):
+    element = get_element(wait, selector, by)
+    element.clear()
+    element.send_keys(str(text))
+
+
+def select_option(wait, selector, value, by=By.ID, mode="value"):
+    element = wait.until(
+        EC.visibility_of_element_located((by, selector))
+    )
+    select = Select(element)
+
+    if mode == "value":
+        select.select_by_value(str(value))
+    elif mode == "text":
+        select.select_by_visible_text(str(value))
+    elif mode == "index":
+        select.select_by_index(int(value))
